ae2935b1a281619d6c024be44c93cfa255b67231
Ivan Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-03-02 01:41:04 +0100

    213

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,31 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    modal: document.querySelector("[data-menu]"),
-  };
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
+    portfolio:document.querySelector('[data-menu-portfolio]'),
+    menuList: document.querySelector('.menu-mobile__menu'),
+    blockPortfolio: document.querySelector('.portfolio'),
+    mainBlock: document.querySelector('.main_block')
+  }; 
+  refs.portfolio.addEventListener('click', getPortfolio)
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuList.addEventListener('click', removeMenu);
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+function getPortfolio(){
+  refs.blockPortfolio.classList.toggle('portfolio_open')
+  refs.mainBlock.classList.toggle('main_block_hidden')
+}
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+
+  function removeMenu() {
+    refs.menu.classList.add('is-hidden');
+    refs.body.classList.remove('no-scroll');
   }
 })();
