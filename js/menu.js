63aa5f6b914504201c2b7e1cc942c045eca80a16
Ivan Andrusyshyn <ivan.andrusyshyn@icloud.com>
2023-03-02 15:28:12 +0100

    213

--- js/menu.js
+++ js/menu.js
@@ -9,13 +9,26 @@
     blockPortfolio: document.querySelector('.portfolio'),
     mainBlock: document.querySelector('.main_block'),
 
-    
-  }; 
+
+    firstLink:document.querySelector('[first]'),
+    secondLink:document.querySelector('[second]')
+
+  };
+  refs.secondLink.addEventListener('click', getSecondLink) 
+  refs.firstLink.addEventListener('click', getFirstLink) 
   refs.portfolio.addEventListener('click', getPortfolio)
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
   refs.menuList.addEventListener('click', removeMenu);
 
+  function getFirstLink(){
+  refs.secondLink.classList.remove('current')
+  }
+  function getSecondLink(){
+    refs.secondLink.classList.add('current')
+    refs.firstLink.classList.remove('current')
+  
+    }
 function getPortfolio(){
   refs.blockPortfolio.classList.toggle('portfolio_open')
   refs.mainBlock.classList.toggle('main_block_hidden')
